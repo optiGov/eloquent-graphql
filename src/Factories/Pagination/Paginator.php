b39adce42e35578192d592b1cb8e56d3c5f3b961
Julius Türich <info@joonlabs.com>
2024-01-06 23:56:19 +0100

    add filter in paginator

--- src/Factories/Pagination/Paginator.php
+++ src/Factories/Pagination/Paginator.php
@@ -2,11 +2,15 @@
 
 namespace EloquentGraphQL\Factories\Pagination;
 
+use EloquentGraphQL\Exceptions\GraphQLError;
+
 abstract class Paginator
 {
     protected ?int $offset = null;
 
     protected ?int $limit = null;
+
+    protected ?array $filter = null;
 
     public function offset(?int $n): static
     {
@@ -18,6 +22,20 @@ abstract class Paginator
     public function limit(?int $n): static
     {
         $this->limit = $n;
+
+        return $this;
+    }
+
+    /**
+     * @throws GraphQLError
+     */
+    public function filter(?array $filter): static
+    {
+        $this->filter = $filter;
+
+        if($filter){
+            $this->applyFilter($filter);
+        }
 
         return $this;
     }
@@ -34,6 +52,14 @@ abstract class Paginator
         $this->limit = null;
 
         return $this;
+    }
+
+    /**
+     * @throws GraphQLError
+     */
+    protected function applyFilter(array $filter): void
+    {
+        throw new GraphQLError('Filtering is not supported for this paginator.');
     }
 
     abstract public function count(): int;
