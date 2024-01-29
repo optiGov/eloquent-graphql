8e4e31c55783070719e41cc0333ed42eca4e30f6
Julius Türich <info@joonlabs.com>
2024-01-29 14:36:12 +0100

    add `FilterCarbon`

---
+++ src/Types/FilterCarbon.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace EloquentGraphQL\Types;
+
+/**
+ * @property ?string $eq
+ *  @property ?string $ne
+ *  @property ?string $lt
+ *  @property ?string $gt
+ *  @property ?string $lte
+ *  @property ?string $gte
+ */
+class FilterCarbon
+{
+}
