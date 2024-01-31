b71aa4c229b58b8009020f5758e18076e9bf3119
Julius Türich <info@joonlabs.com>
2024-01-31 19:43:32 +0100

    add proper parsing of values

--- src/Types/CarbonType.php
+++ src/Types/CarbonType.php
@@ -14,30 +14,30 @@ class CarbonType extends ScalarType
 {
     public string $name = 'DateTime';
 
-    /**
-     * @throws Error
-     */
     public function serialize($value)
     {
+        // check if value is instance of Carbon
         if (! ($value instanceof Carbon)) {
-            throw new InvariantViolation('Could not serialize following value as Carbon: '.Utils::printSafe($value));
-        }
-
-        return $this->parseValue($value);
-    }
-
-    public function parseValue($value)
-    {
-        if (! ($value instanceof Carbon)) {
-            throw new Error('Cannot represent following value as email: '.Utils::printSafeJson($value));
+            throw new InvariantViolation('Could not serialize following value as datetime: '.Utils::printSafe($value).'. A Carbon object is expected.');
         }
 
         return $value->format('Y-m-d H:i:s');
     }
 
-    public function parseLiteral(Node $valueNode, array $variables = null)
+    public function parseValue($value)
     {
-        // Throw GraphQL\Error\Error vs \UnexpectedValueException to locate the error in the query
+        // check if value is parsable as date
+        $date = Carbon::parse($value);
+        if (! $date) {
+            throw new Error('Cannot represent following value as timestamp: '.Utils::printSafeJson($value));
+        }
+
+        return $date->format('Y-m-d H:i:s');
+    }
+
+    public function parseLiteral(Node $valueNode, ?array $variables = null)
+    {
+        // throw GraphQL\Error\Error vs \UnexpectedValueException to locate the error in the query
         if (! $valueNode instanceof StringValueNode) {
             throw new Error('Query error: Can only parse strings got: '.$valueNode->kind, [$valueNode]);
         }
