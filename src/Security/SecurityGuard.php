d2d350aad1e241082bf84d7854d7e12d673ee222
Julius Türich <info@joonlabs.com>
2024-02-07 10:42:02 +0100

    allow non Model objects to be checked

--- src/Security/SecurityGuard.php
+++ src/Security/SecurityGuard.php
@@ -7,7 +7,6 @@ use EloquentGraphQL\Reflection\ReflectionProperty;
 use EloquentGraphQL\Services\EloquentGraphQLService;
 use Illuminate\Contracts\Auth\Access\Gate;
 use Illuminate\Contracts\Container\BindingResolutionException;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection;
 use ReflectionException;
 
@@ -69,7 +68,7 @@ class SecurityGuard
      * @throws BindingResolutionException
      * @throws GraphQLError
      */
-    public function assertCanDelete(Model $model): void
+    public function assertCanDelete(object $model): void
     {
         if (! $this->check('delete', $model::class, [$model])) {
             throw new GraphQLError('You are not authorized to delete this model.');
@@ -80,7 +79,7 @@ class SecurityGuard
      * @throws BindingResolutionException
      * @throws GraphQLError
      */
-    public function assertCanUpdate(Model $model, array $data): void
+    public function assertCanUpdate(object $model, array $data): void
     {
         if (! $this->check('update', $model::class, [$model, $data])) {
             throw new GraphQLError('You are not authorized to update this model.');
@@ -91,7 +90,7 @@ class SecurityGuard
      * @throws BindingResolutionException
      * @throws GraphQLError
      */
-    public function assertCanViewProperty(Model $model, ReflectionProperty $property): void
+    public function assertCanViewProperty(object $model, ReflectionProperty $property): void
     {
         if (! $this->check('viewProperty', $model::class, [$model, $property->getName()])) {
             throw new GraphQLError('You are not authorized to view this property.');
@@ -102,7 +101,7 @@ class SecurityGuard
      * @throws BindingResolutionException
      * @throws GraphQLError
      */
-    public function assertCanView(Model $model): void
+    public function assertCanView(object $model): void
     {
         if (! $this->check('view', $model::class, [$model])) {
             throw new GraphQLError('You are not authorized to view this model.');
@@ -132,6 +131,6 @@ class SecurityGuard
             $models = collect($models);
         }
 
-        return $models->each(fn (Model $model) => $this->assertCanView($model));
+        return $models->each(fn (object $model) => $this->assertCanView($model));
     }
 }
